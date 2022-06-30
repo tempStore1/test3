97ff3d39d2a34c78932c9215b0662295cc8df767
BardKidd <mobilesuit0922@gmail.com>
2022-06-30 23:56:23 +0800

    feat: 調整 Starter Kit 封面

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 const App: React.FC = () => {
   return (
-    <div className="starterkit-container starterkit-text-center">
-      Hello World
+    <div className=" demo-text-white demo-h-screen  demo-bg-slate-700	 demo-pt-10	demo-w-full">
+      <div className="demo-container demo-p-4 demo-border demo-shadow demo-w-[1024px] demo-mx-auto demo-rounded demo-text-center">
+        <h1 className="demo-text-2xl">Hello World</h1>
+        <p>This is a sample Starter Kit !</p>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,7 @@
 module.exports = {
-  prefix: "starterkit-",
+  prefix: "demo-",
   content: ["./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
