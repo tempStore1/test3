948567e13aa8bee8e2c7c1c97d52afa32dd71b5d
BardKidd <mobilesuit0922@gmail.com>
2022-07-02 12:07:58 +0800

    feat: 新增 font awesome 套件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
+
 const App: React.FC = () => {
   return (
-    <div className=" demo-text-white demo-h-screen  demo-bg-slate-700	 demo-pt-10	demo-w-full">
-      <div className="demo-container demo-p-4 demo-border demo-shadow demo-w-[1024px] demo-mx-auto demo-rounded demo-text-center">
-        <h1 className="demo-text-2xl">Hello World</h1>
-        <p>This is a sample Starter Kit !</p>
+    <div className="demo-relative demo-w-full demo-text-white demo-h-screen  demo-bg-slate-700">
+      <div className="demo-absolute demo-top-1/2 demo-left-1/2 demo--translate-y-1/2 demo--translate-x-1/2 demo-w-[1024px]">
+        <div className="demo-container demo-p-4 demo-border demo-shadow  demo-mx-auto demo-rounded demo-text-center">
+          <h1 className="demo-text-2xl">Hello World</h1>
+          <p>This is a sample Starter Kit !</p>
+        </div>
+        <div className="demo-mx-auto demo-text-center demo-mt-5">
+          <a
+            target="_blank"
+            href="https://github.com/BardKidd/base-starter-kit"
+            className="demo-border demo-inline-block  demo-rounded demo-p-3 demo-bg-white demo-cursor-pointer demo-ease-in-out demo-duration-150 hover:demo-ring-4	"
+          >
+            <FontAwesomeIcon
+              icon={brands("github")}
+              className="demo-text-slate-700 demo-pr-2 demo-text-2xl"
+            />
+            <button
+              type="button"
+              className="demo-text-slate-700 demo-font-bold demo-text-2xl"
+            >
+              GitHub
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
