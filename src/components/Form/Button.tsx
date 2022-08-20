97e3c38a6764b5f84b65d1520b14d284bf82bb78
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-08-20 11:55:57 +0800

    fix: Button 元件 props 修正

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -9,9 +9,10 @@ const Button = styled.button<ButtonProps>`
   padding: 12px 48px;
   background: ${({ isReverse }) => (isReverse ? "transparent" : "#333")};
   border-radius: 10px;
-  color: ${(isReverse) => (isReverse ? "#333" : "#fff")};
+  color: ${({ isReverse }) => (isReverse ? "#333" : "#fff")};
   font-weight: 700;
   margin: 0 auto;
 `;
+Button.displayName = "Button";
 
 export default Button;
