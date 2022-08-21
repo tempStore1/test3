55ff884f842b96e85538fca46212a620b134fad1
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-08-21 18:17:12 +0800

    feat: 取得 token

--- src/pages/Home/Login/LoginResource.ts
+++ src/pages/Home/Login/LoginResource.ts
@@ -2,13 +2,21 @@ const userLogin = async (user: { email: string; password: string }) => {
   const res = await fetch(`${process.env.API_PATH}/users/sign_in`, {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ user }),
   });
-  if (res.status === 200) {
+
+  const { status, headers } = res;
+
+  if (status === 200) {
     const data = await res.json();
     const { message } = data;
+    const token = headers.get("authorization");
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+    localStorage.setItem("token", token);
+
     return message;
   } else {
     const data = await res.json();
